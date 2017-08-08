ae866c98d1386d58244766b39ae0b535d5c0a5b1
yaroslav <yaroslav@m3-dev.flavt>
2017-08-08 16:48:36 +0400

    Версия без изменения Сделал константы static'ами

--- src/flavt/slg/lib/constants/SLG_Constants.java
+++ src/flavt/slg/lib/constants/SLG_Constants.java
@@ -11,10 +11,10 @@ package flavt.slg.lib.constants;
  * @author yaroslav
  */
 public class SLG_Constants {
-    public final int SLG_CMD_DEV_TYPE_M3A1 = 0x01;
-    public final int SLG_CMD_DEV_TYPE_M3A2 = 0x02;
-    public final int SLG_CMD_DEV_TYPE_M3A3 = 0x03;
+    public static final int SLG_CMD_DEV_TYPE_M3A1 = 0x01;
+    public static final int SLG_CMD_DEV_TYPE_M3A2 = 0x02;
+    public static final int SLG_CMD_DEV_TYPE_M3A3 = 0x03;
     
-    public final int SLG_CMD_DEV_TYPE_M3B1 = 0x80;
-    public final int SLG_CMD_DEV_TYPE_M3B2 = 0x81;
+    public static final int SLG_CMD_DEV_TYPE_M3B1 = 0x80;
+    public static final int SLG_CMD_DEV_TYPE_M3B2 = 0x81;
 }
--- src/flavt/slg/lib/constants/SLG_ConstantsCmd.java
+++ src/flavt/slg/lib/constants/SLG_ConstantsCmd.java
@@ -11,22 +11,22 @@ package flavt.slg.lib.constants;
  * @author yaroslav
  */
 public class SLG_ConstantsCmd {
-    public final int SLG_CMD_SET = 0x21;                           //33 = 0x21 = "!"
-    public final int SLG_CMD_REQ = 0x22;                           //34 = 0x22 = """
+    public static final int SLG_CMD_SET = 0x21;                           //33 = 0x21 = "!"
+    public static final int SLG_CMD_REQ = 0x22;                           //34 = 0x22 = """
     
-    public final int SLG_CMD_ACT_SAVE_FLASH_PARAM       = 0x23;    //35 = 0x23 = "#"    Param: 0-1-2-3  - page to save
-    public final int SLG_CMD_ACT_RELOAD_FLASH_PARAM     = 0x24;    //36 = 0x24 = "$"    Param: 0-1-2-3  - page to reload
+    public static final int SLG_CMD_ACT_SAVE_FLASH_PARAM       = 0x23;    //35 = 0x23 = "#"    Param: 0-1-2-3  - page to save
+    public static final int SLG_CMD_ACT_RELOAD_FLASH_PARAM     = 0x24;    //36 = 0x24 = "$"    Param: 0-1-2-3  - page to reload
     
-    public final int SLG_CMD_ACT_T_CALIBRATION          = 0x25;    //37 = 0x25 = "%"
-    public final int SLG_CMD_ACT_RESET_T_CALIB          = 0x26;    //38 = 0x26 = "&"
+    public static final int SLG_CMD_ACT_T_CALIBRATION          = 0x25;    //37 = 0x25 = "%"
+    public static final int SLG_CMD_ACT_RESET_T_CALIB          = 0x26;    //38 = 0x26 = "&"
 
-    public final int SLG_CMD_ACT_LASER_OFF              = 0x27;    //39 = 0x27 = "'"
-    public final int SLG_CMD_ACT_INTEGR_OFF             = 0x28;    //40 = 0x28 = "("
-    public final int SLG_CMD_ACT_INTEGR_ON              = 0x29;    //41 = 0x29 = ")"
-    public final int SLG_CMD_ACT_INTEGR_RESET           = 0x2A;    //42 = 0x2A = "*"
+    public static final int SLG_CMD_ACT_LASER_OFF              = 0x27;    //39 = 0x27 = "'"
+    public static final int SLG_CMD_ACT_INTEGR_OFF             = 0x28;    //40 = 0x28 = "("
+    public static final int SLG_CMD_ACT_INTEGR_ON              = 0x29;    //41 = 0x29 = ")"
+    public static final int SLG_CMD_ACT_INTEGR_RESET           = 0x2A;    //42 = 0x2A = "*"
 
-    public final int SLG_CMD_ACT_SWC_DW_DNDU_OUTPUT     = 0x2B;    //43 = 0x2B = "+"
+    public static final int SLG_CMD_ACT_SWC_DW_DNDU_OUTPUT     = 0x2B;    //43 = 0x2B = "+"
 
-    public final int SLG_CMD_ACT_LOCK_DEVICE            = 0x2C;    //44 = 0x2C = ","
-    public final int SLG_CMD_ACT_UNLOCK_DEVICE          = 0x2D;    //45 = 0x2D = "-"
+    public static final int SLG_CMD_ACT_LOCK_DEVICE            = 0x2C;    //44 = 0x2C = ","
+    public static final int SLG_CMD_ACT_UNLOCK_DEVICE          = 0x2D;    //45 = 0x2D = "-"
 }
