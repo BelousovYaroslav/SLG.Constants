964968d1a4bbfebf7548e8a37c4419fd3058d1eb
BelousovYaroslav <belousov.yaroslav@gmail.com>
2017-11-23 15:03:22 +0400

    Версия без изменений Добавил константы неопределённого типа прибора (м3а-м3б-!!хз!!) и неопределённого типа режима работы прибора (синх-асинх-!!хз!!)

--- src/flavt/slg/lib/constants/SLG_Constants.java
+++ src/flavt/slg/lib/constants/SLG_Constants.java
@@ -11,13 +11,15 @@ package flavt.slg.lib.constants;
  * @author yaroslav
  */
 public class SLG_Constants {
-    public static final int SLG_CMD_DEV_TYPE_M3A1 = 0x01;
-    public static final int SLG_CMD_DEV_TYPE_M3A2 = 0x02;
-    public static final int SLG_CMD_DEV_TYPE_M3A3 = 0x03;
+    public static final int SLG_CMD_DEV_TYPE_UNKNOWN    = 0x00;
+    public static final int SLG_CMD_DEV_TYPE_M3A1       = 0x01;
+    public static final int SLG_CMD_DEV_TYPE_M3A2       = 0x02;
+    public static final int SLG_CMD_DEV_TYPE_M3A3       = 0x03;
     
-    public static final int SLG_CMD_DEV_TYPE_M3B1 = 0x80;
-    public static final int SLG_CMD_DEV_TYPE_M3B2 = 0x81;
+    public static final int SLG_CMD_DEV_TYPE_M3B1       = 0x80;
+    public static final int SLG_CMD_DEV_TYPE_M3B2       = 0x81;
     
-    public static final int SLG_REGIME_SYNC = 0x01;
-    public static final int SLG_REGIME_ASYNC = 0x02;
+    public static final int SLG_REGIME_SYNC         = 0x01;
+    public static final int SLG_REGIME_ASYNC        = 0x02;
+    public static final int SLG_REGIME_UNKNOWN      = 0x03;
 }
